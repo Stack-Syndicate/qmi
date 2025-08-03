e2c45ad8cc712adca71c67d2816880ab432622f5
Glenn Wallace <glennw-coding@proton.me>
2025-08-03 16:11:31 +0200

    Added a couple of empty structs and a trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,18 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+trait Component {}
+struct Query {}
+struct Mutation {}
+struct Interaction {}
+
+struct QMIWorld {
+    entities: Vec<usize>
+}
+impl QMIWorld {
+    fn new() -> Self {
+        todo!("Return empty QMI World")
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    
 }
